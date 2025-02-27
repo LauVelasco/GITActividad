fbabcb20a10537d9f2b32e72024abf0176eb8812
Lau Velasco <laurajhulianavel@gmail.com>
2025-02-26 21:38:47 -0500

    salidas por consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Buen día");
+        System.out.printf("Estudiando Git");
+        System.out.printf("Estudiando Git");
 
 
     }
