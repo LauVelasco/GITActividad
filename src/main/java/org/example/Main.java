0160b7a16697fa07745fcd2b57118cc74031a1fa
Lau Velasco <laurajhulianavel@gmail.com>
2025-02-27 12:18:26 -0500

    Comandos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Buen día");
+        System.out.println("Buen día");
+        System.out.println("Estudiando Git");
+        System.out.printf("Estudiando Git");
+        System.out.printf("Estudiando Git");
+        System.out.printf("Estudiando Git");
         System.out.printf("Estudiando Git");
         System.out.printf("Estudiando Git");
 
